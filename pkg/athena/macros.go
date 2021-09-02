a9d7010ba094f87a1bdaedc24c22264f72be0057
Andres Martinez Gotor <andres.mgotor@gmail.com>
2021-09-02 15:10:52 +0200

    macro

--- pkg/athena/macros.go
+++ pkg/athena/macros.go
@@ -1,10 +1,29 @@
 package athena
 
 import (
+	"fmt"
+
 	"github.com/grafana/sqlds/v2"
+	"github.com/pkg/errors"
 )
 
-var macros = map[string]sqlds.MacroFunc{}
+func macroDateFilter(query *sqlds.Query, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", errors.WithMessagef(sqlds.ErrorBadArgumentCount, "expected 1 argument, received %d", len(args))
+	}
+
+	var (
+		column = args[0]
+		from   = query.TimeRange.From.UTC().Format("2006-01-02")
+		to     = query.TimeRange.To.UTC().Format("2006-01-02")
+	)
+
+	return fmt.Sprintf("%s BETWEEN date '%s' AND date '%s'", column, from, to), nil
+}
+
+var macros = map[string]sqlds.MacroFunc{
+	"dateFilter": macroDateFilter,
+}
 
 func (s *AthenaDatasource) Macros() sqlds.Macros {
 	return macros

eba0a4a193f18732522a269b48d3d44e617fd06c
Andres Martinez Gotor <andres.martinez@grafana.com>
2021-09-20 12:00:55 +0200

    Add macros (#56)

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/aws/aws-sdk-go v1.37.32
 	github.com/google/go-cmp v0.5.5
 	github.com/grafana/grafana-aws-sdk v0.7.0
-	github.com/grafana/grafana-plugin-sdk-go v0.110.0
-	github.com/grafana/sqlds/v2 v2.1.1-0.20210909150504-1c981c05fd07
+	github.com/grafana/grafana-plugin-sdk-go v0.114.0
+	github.com/grafana/sqlds/v2 v2.2.0
 	github.com/jpillora/backoff v1.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
--- pkg/athena/macros.go
+++ pkg/athena/macros.go
@@ -2,10 +2,89 @@ package athena
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend/gtime"
 	"github.com/grafana/sqlds/v2"
 	"github.com/pkg/errors"
 )
+
+const (
+	timestampFormat   = "'yyyy-MM-dd HH:mm:ss'"
+	goTimestampFormat = "2006-01-02 15:04:05"
+)
+
+func parseTime(target, format string) string {
+	if format == "" {
+		return target
+	} else if format == timestampFormat {
+		return fmt.Sprintf("TIMESTAMP %s", target)
+	}
+	return fmt.Sprintf("parse_datetime(%s,%s)", target, format)
+}
+
+func macroTimeGroup(query *sqlds.Query, args []string) (string, error) {
+	if len(args) < 2 {
+		return "", errors.WithMessagef(sqlds.ErrorBadArgumentCount, "macro $__timeGroup needs time column and interval")
+	}
+
+	interval, err := gtime.ParseInterval(strings.Trim(args[1], `'`))
+	if err != nil {
+		return "", fmt.Errorf("error parsing interval %v", args[1])
+	}
+
+	timeVar := args[0]
+	if len(args) == 3 {
+		timeVar = parseTime(args[0], args[2])
+	}
+	return fmt.Sprintf("FROM_UNIXTIME(FLOOR(TO_UNIXTIME(%s)/%v)*%v)", timeVar, interval.Seconds(), interval.Seconds()), nil
+}
+
+func macroParseTime(query *sqlds.Query, args []string) (string, error) {
+	if len(args) < 1 {
+		return "", errors.WithMessagef(sqlds.ErrorBadArgumentCount, "expected at least one argument")
+	}
+
+	var (
+		column     = args[0]
+		timeFormat = timestampFormat
+	)
+
+	if len(args) == 2 {
+		timeFormat = args[1]
+	}
+
+	return parseTime(column, timeFormat), nil
+}
+
+func macroTimeFilter(query *sqlds.Query, args []string) (string, error) {
+	if len(args) < 1 {
+		return "", errors.WithMessagef(sqlds.ErrorBadArgumentCount, "expected at least one argument")
+	}
+
+	var (
+		column     = args[0]
+		timeFormat = ""
+		from       = query.TimeRange.From.Format(goTimestampFormat)
+		to         = query.TimeRange.To.Format(goTimestampFormat)
+	)
+
+	if len(args) > 1 {
+		timeFormat = args[1]
+	}
+	timeVar := parseTime(column, timeFormat)
+
+	return fmt.Sprintf("%s BETWEEN TIMESTAMP '%s' AND TIMESTAMP '%s'", timeVar, from, to), nil
+}
+
+func macroTimeFrom(query *sqlds.Query, args []string) (string, error) {
+	return fmt.Sprintf("TIMESTAMP '%s'", query.TimeRange.From.UTC().Format(goTimestampFormat)), nil
+
+}
+
+func macroTimeTo(query *sqlds.Query, args []string) (string, error) {
+	return fmt.Sprintf("TIMESTAMP '%s'", query.TimeRange.To.UTC().Format(goTimestampFormat)), nil
+}
 
 func macroDateFilter(query *sqlds.Query, args []string) (string, error) {
 	if len(args) != 1 {
@@ -23,6 +102,11 @@ func macroDateFilter(query *sqlds.Query, args []string) (string, error) {
 
 var macros = map[string]sqlds.MacroFunc{
 	"dateFilter": macroDateFilter,
+	"parseTime":  macroParseTime,
+	"timeFilter": macroTimeFilter,
+	"timeFrom":   macroTimeFrom,
+	"timeGroup":  macroTimeGroup,
+	"timeTo":     macroTimeTo,
 }
 
 func (s *AthenaDatasource) Macros() sqlds.Macros {

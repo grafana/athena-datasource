81c2a0578db5d5da1519b92d021d50d3d5797cb9
Andres Martinez Gotor <andres.mgotor@gmail.com>
2021-10-15 10:57:35 +0200

    update API paths

--- src/QueryEditor.test.tsx
+++ src/QueryEditor.test.tsx
@@ -87,7 +87,7 @@ describe('QueryEditor', () => {
 
     await select(selectEl, 'foo', { container: document.body });
 
-    expect(ds.postResource).toHaveBeenCalledWith('tablesWithConnectionDetails', { ...q.connectionArgs });
+    expect(ds.postResource).toHaveBeenCalledWith('tables', { ...q.connectionArgs });
     expect(onChange).toHaveBeenCalledWith({
       ...q,
       table: 'foo',
@@ -113,7 +113,7 @@ describe('QueryEditor', () => {
 
     await select(selectEl, 'columnName', { container: document.body });
 
-    expect(ds.postResource).toHaveBeenCalledWith('columnsWithConnectionDetails', {
+    expect(ds.postResource).toHaveBeenCalledWith('columns', {
       ...q.connectionArgs,
       table: 'tableName',
     });

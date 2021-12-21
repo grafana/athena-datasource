2c3d7327cac2aa29bacbb8cce43749613010df9c
Andres Martinez Gotor <andres.mgotor@gmail.com>
2021-12-21 17:01:32 +0100

    fix e2e tests

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -40,20 +40,10 @@ e2e.scenario({
           checkHealth: false,
           expectedAlertMessage: 'Data source is working',
           form: () => {
-            e2e()
-              .get('.aws-config-authType')
-              .find(`input`)
-              .click({ force: true })
-              .type('Access & secret key')
-              .type('{enter}');
+            e2eSelectors.ConfigEditor.AuthenticationProvider.input().type('Access & secret key').type('{enter}');
             e2eSelectors.ConfigEditor.AccessKey.input().type(datasource.secureJsonData.accessKey);
             e2eSelectors.ConfigEditor.SecretKey.input().type(datasource.secureJsonData.secretKey);
-            e2e()
-              .get('.aws-config-defaultRegion')
-              .find(`input`)
-              .click({ force: true })
-              .type(datasource.jsonData.defaultRegion)
-              .type('{enter}');
+            e2eSelectors.ConfigEditor.DefaultRegion.input().type(datasource.jsonData.defaultRegion).type('{enter}');
             // Catalogs
             e2eSelectors.ConfigEditor.catalog.input().click({ force: true });
             // wait for it to load

99acf87766382aea0cb4bb1b376658bd6fa7ed57
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2023-10-31 12:45:01 +0100

    Add to spellcheck and add comment

--- pkg/main.go
+++ pkg/main.go
@@ -21,6 +21,9 @@ func main() {
 	ds.EnableMultipleConnections = true
 	ds.CustomRoutes = routes.New(s).Routes()
 
+	// newDatasourceForUpgradedPluginSdk adds context to the NewDatasource function, which is the new signature expected
+	// for an InstanceFactoryFunc in  grafana-plugin-sdk-go v0.177.0. Moving forward, we can update the NewDatasource
+	// signature in grafana-aws-sdk once all dependent datasources upgrade their grafana-plugin-sdk-go.
 	newDatasourceForUpgradedPluginSdk := func(ctx context.Context, settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
 		return ds.NewDatasource(settings)
 	}

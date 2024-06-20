db15450c6cfdb70044e29e417dc4fde8c0874526
Kevin Yu <kevinwcyu@users.noreply.github.com>
2024-06-20 13:54:05 -0700

    try waiting

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -57,11 +57,8 @@ e2e.scenario({
             // Catalogs
             e2eSelectors.ConfigEditor.catalog.input().click({ force: true });
             // wait for it to load
-            cy.contains(
-              `[data-testid="${selectors.components.ConfigEditor.catalog.wrapper}"]`,
-              datasource.jsonData.catalog
-            );
-            // e2eSelectors.ConfigEditor.catalog.wrapper().contains(datasource.jsonData.catalog);
+            cy.wait(2000);
+            e2eSelectors.ConfigEditor.catalog.wrapper().contains(datasource.jsonData.catalog);
             e2eSelectors.ConfigEditor.catalog.input().type(datasource.jsonData.catalog).type('{enter}');
             // Databases
             e2eSelectors.ConfigEditor.database.input().click({ force: true });

c2b50aec6a7c94ba259218fc43c25a25df9dd979
Kevin Yu <kevinwcyu@users.noreply.github.com>
2024-06-20 14:27:20 -0700

    update yarn.lock file

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -57,7 +57,7 @@ e2e.scenario({
             // Catalogs
             e2eSelectors.ConfigEditor.catalog.input().click({ force: true });
             // wait for it to load
-            e2eSelectors.ConfigEditor.catalog.wrapper().first().contains(datasource.jsonData.catalog);
+            e2eSelectors.ConfigEditor.catalog.wrapper().contains(datasource.jsonData.catalog);
             e2eSelectors.ConfigEditor.catalog.input().type(datasource.jsonData.catalog).type('{enter}');
             // Databases
             e2eSelectors.ConfigEditor.database.input().click({ force: true });

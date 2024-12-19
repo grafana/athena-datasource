57bf178b0b529b83bb3854d688a8cf6828726981
idastambuk <ida.stambuk@grafana.com>
2024-12-19 16:09:56 +0100

    Add playwright tests

--- tests/annotationsEditor.spec.ts
+++ tests/annotationsEditor.spec.ts
@@ -4,5 +4,16 @@ import { selectors } from '../src/tests/selectors';
 test('should render annotations editor', async ({ annotationEditPage, page }) => {
   const regionSelector = selectors.components.ConfigEditor.region.wrapper;
   await annotationEditPage.datasource.set('AWS Athena');
-  await expect(page.getByTestId(regionSelector)).toBeVisible();
+
+  // Wait for the monaco editor to finish lazy loading
+  await page.waitForFunction(() => window.monaco);
+
+  await annotationEditPage.getByGrafanaSelector(selectors.components.QueryEditor.CodeEditor.container).click();
+  await page.keyboard.insertText(`select * from cloudfront_logs where bytes < 100`);
+  await expect(annotationEditPage.runQuery()).toBeOK();
+  const table = page.locator('.filter-table');
+  const timeDropdown = table.getByText('time', { exact: true }).locator('..').locator('input').locator('..');
+  await timeDropdown.click();
+  const datetime = page.getByText('date (time)');
+  expect(datetime).toBeVisible();
 });
--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -1,7 +1,40 @@
 import { test, expect } from '@grafana/plugin-e2e';
-
+import { selectors } from '../src/tests/selectors';
+import { DatasourceWithAsyncBackend } from '@grafana/async-query-data';
+import { AthenaDataSourceOptions, AthenaDataSourceSecureJsonData } from '../src/types';
 test('should render config editor', async ({ createDataSourceConfigPage, readProvisionedDataSource, page }) => {
-  const ds = await readProvisionedDataSource({ fileName: 'aws-athena.yaml', name: 'AWS Athena' });
-  await createDataSourceConfigPage({ type: ds.type });
+  const datasource = await readProvisionedDataSource<AthenaDataSourceOptions, AthenaDataSourceSecureJsonData>({
+    fileName: 'aws-athena.yaml',
+    name: 'AWS Athena',
+  });
+  const { defaultRegion, catalog, database, workgroup } = datasource.jsonData;
+  await createDataSourceConfigPage({ type: datasource.type });
   await expect(page.getByTestId('connection-config')).toBeVisible();
+  await page.getByRole('combobox', { name: selectors.components.ConfigEditor.AuthenticationProvider.input }).click();
+  await page.getByText('Access & secret key').click();
+  if (datasource.secureJsonData?.accessKey && datasource.secureJsonData?.secretKey) {
+    // Authenticate
+    await page.getByLabel(selectors.components.ConfigEditor.AccessKey.input).fill(datasource.secureJsonData.accessKey);
+    await page.getByLabel(selectors.components.ConfigEditor.SecretKey.input).fill(datasource.secureJsonData.secretKey);
+  } else {
+    throw new Error('Missing one of secureJsonData: accessKey, secretKey in the datasource');
+  }
+  expect(page.getByLabel(selectors.components.ConfigEditor.AccessKey.input)).toBeVisible();
+  expect(page.getByLabel(selectors.components.ConfigEditor.SecretKey.input)).toBeVisible();
+  if (defaultRegion && catalog && database && workgroup) {
+    // Default region
+    await page.getByRole('combobox', { name: selectors.components.ConfigEditor.DefaultRegion.input }).click();
+    await page.getByText(defaultRegion).click();
+    // Catalogs
+    await page.getByRole('combobox', { name: selectors.components.ConfigEditor.catalog.input }).click();
+    await page.getByText(catalog).click();
+    // Databases
+    await page.getByRole('combobox', { name: selectors.components.ConfigEditor.database.input }).click();
+    await page.getByText(database).click();
+    // Workgroups
+    await page.getByRole('combobox', { name: selectors.components.ConfigEditor.workgroup.input }).click();
+    await page.getByText(workgroup).click();
+  } else {
+    throw new Error('Missing one of default data: defaultRegion, catalog, database, workgroup in the datasource');
+  }
 });

a32573c1bdd68e624b2fd917d5ec5da13ea11f4a
Ida Štambuk <ida.stambuk@grafana.com>
2025-01-13 10:25:16 +0100

    MigrateCypress tests to Playwright (#482)

--- tests/annotationsEditor.spec.ts
+++ tests/annotationsEditor.spec.ts
@@ -2,7 +2,15 @@ import { expect, test } from '@grafana/plugin-e2e';
 import { selectors } from '../src/tests/selectors';
 
 test('should render annotations editor', async ({ annotationEditPage, page }) => {
-  const regionSelector = selectors.components.ConfigEditor.region.wrapper;
   await annotationEditPage.datasource.set('AWS Athena');
-  await expect(page.getByTestId(regionSelector)).toBeVisible();
+
+  // Wait for the monaco editor to finish lazy loading
+  await page.waitForFunction(() => window.monaco);
+
+  await annotationEditPage.getByGrafanaSelector(selectors.components.QueryEditor.CodeEditor.container).click();
+  await page.keyboard.insertText(`select * from cloudfront_logs where bytes < 100`);
+  await expect(annotationEditPage.runQuery()).toBeOK();
+  const timeDropdown = page.getByText('time, or the first time field', { exact: true });
+  await timeDropdown.click({ force: true });
+  await expect(page.getByText('date (time)', { exact: true })).toBeVisible();
 });

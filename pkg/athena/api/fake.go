e8ef13617f2a0f17f6b7e629433558e479464a68
Nathan Verzemnieks <njvrzm@gmail.com>
2025-01-16 14:20:00 +0100

    Update for aws-sdk-go-v2

--- pkg/athena/api/fake.go
+++ pkg/athena/api/fake.go
@@ -1,11 +1,10 @@
 package api
 
 import (
-	"github.com/aws/aws-sdk-go/service/athena/athenaiface"
 	"github.com/grafana/athena-datasource/pkg/athena/models"
 )
 
 // NewFake returns an API object with the given args
-func NewFake(cli athenaiface.AthenaAPI, settings *models.AthenaDataSourceSettings) *API {
+func NewFake(cli Client, settings *models.AthenaDataSourceSettings) *API {
 	return &API{Client: cli, settings: settings}
 }

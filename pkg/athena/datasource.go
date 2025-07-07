5ddc71f90235436d5e180000def7a9f64902b8ae
Kevin Yu <kevinwcyu@users.noreply.github.com>
2025-07-07 07:36:12 -0700

    update github.com/grafana/grafana-aws-sdk to v1.0.2 (#586)

--- pkg/athena/datasource.go
+++ pkg/athena/datasource.go
@@ -52,8 +52,8 @@ func (l Loader) LoadSettings(ctx context.Context) sqlModels.Settings {
 	return models.New(ctx)
 }
 
-func (l Loader) LoadAPI(ctx context.Context, cache *awsds.SessionCache, settings sqlModels.Settings) (sqlAPI.AWSAPI, error) {
-	return api.New(ctx, cache, settings)
+func (l Loader) LoadAPI(ctx context.Context, settings sqlModels.Settings) (sqlAPI.AWSAPI, error) {
+	return api.New(ctx, settings)
 }
 
 func (l Loader) LoadDriver(ctx context.Context, awsapi sqlAPI.AWSAPI) (awsDriver.Driver, error) {
